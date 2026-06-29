961abd06aa5fbaac2cd18216dfb839501764796a
unknown <mashoodmachu535@gmail.com>
2026-06-29 20:01:32 +0530

    Configure Render Deployment Settings

--- plantproject/settings.py
+++ plantproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i0t%=_=6sjuu=pi$_xv$v^g)@tan@fvkhhkk3@k787#__b^qr2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
